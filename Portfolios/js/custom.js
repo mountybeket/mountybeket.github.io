749a4fbad99967815ee85a6a3e52b4eaf2b260a4
beketalimusin <kokotokokoto0@gmail.com>
2017-06-23 10:33:08 +0600

    new portfolios

---
+++ Portfolios/js/custom.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+
+function Rotator(start_from){
+    var phrases = ["фывфывфывфывыв","EVSVSGBTRSVDSXZ","VHRCSDGRWERSGFDS"];
+    var total = phrases.length;
+    var interval = 1000;
+    if(void 0 === start_from){
+        start_from = 0;
+    }
+
+    $(".textrotator").text(phrases[start_from]).animate({"opacity":"1"}, 1000, function(){
+                if(start_from >= (total-1)){
+                    setTimeout(function(){
+                        $(".textrotator").animate({"opacity":"0"}, 1000, function(){
+                            Rotator();
+                        });
+                    }, interval);
+                }else{
+                    start_from++;
+                    setTimeout(function(){
+                        $(".textrotator").animate({"opacity":"0"}, 1000,function(){
+                            Rotator(start_from);
+                        });
+                    }, interval);
+
+                }
+
+    })
+}
+
+Rotator();
+})
